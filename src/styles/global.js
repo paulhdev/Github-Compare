b9a3e23fbbc47dbdefbabf019f155ace94e3fecd
PauloRev <paulohenriquerev@gmail.com>
2019-06-23 23:22:46 -0300

    Added ComponentList style

--- src/styles/global.js
+++ src/styles/global.js
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
     background: #9b75e6;
     text-rendering: optimizeLegibility !important;
     -webkit-font-smoothing: antialiased !important;
+    font-family: sans-serif;
   }
 `;
 
