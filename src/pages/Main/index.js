191f701b705b618fe4216baa4f961799a1bbe046
PauloRev <paulohenriquerev@gmail.com>
2019-06-24 00:04:05 -0300

    Added axios and made requisition to API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
+
 import logo from '../../assets/images/logo.png';
 
 import { Container, Form } from './styles';
-
 import CompareList from '../../components/CompareList';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Logo GitCompare" />
+export default class Main extends Component {
+  state = {
+    repositorieInput: '',
+    repositories: [],
+  };
 
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+  handleAddRepository = async (e) => {
+    e.preventDefault();
 
-export default Main;
+    try {
+      const response = await api.get(`/repos/${this.state.repositorieInput}`);
+
+      this.setState({
+        repositorieInput: '',
+        repositories: [...this.state.repositories, response.data],
+      });
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Logo GitCompare" />
+
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repositorieInput}
+            onChange={e => this.setState({ repositorieInput: e.target.value })}
+          />
+          <button type="submit">OK</button>
+        </Form>
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
